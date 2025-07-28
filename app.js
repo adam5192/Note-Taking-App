6ec4b972042323a079df6a9193cdfbfb18444a76
adam5192 <adammokdad2003@gmail.com>
2025-07-28 17:00:02 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+let notes = []
+
+function openNoteDialog() {
+    console.log("openNoteDialog() called");
+    const dialog = document.getElementById('noteDialog');
+    const titleInput = document.getElementById('noteTitle');
+    const contentInput = document.getElementById('noteContent');
+    
+    dialog.showModal();
+    titleInput.focus();
+}
